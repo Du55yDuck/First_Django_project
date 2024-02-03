0f6cfaff93b2b319ad9279e1f4abfcd30992dd7e
Denis Riabov <zwx7000@mail.ru>
2024-02-03 20:20:12 +0400

    Добавить динамические URL(cats/2/, slug/), Пользовательский конвертер FourDigitYearCOnverter, создать представление для archive

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -8,5 +8,13 @@ def index(request):  # request - ссылка на запрос HttpRequest
     return HttpResponse("Страница приложения women")  # вернет экземпляр класса с ответом
 
 
-def categories(request):  # вторая ф-я представления
-    return HttpResponse("<h1>Статьи по категориям</h1>")  # возвращает экземпляр с ответом
+def categories(request, cat_id):  # вторая ф-я представления + параметр cat_id принимающие целые числа в конце адреса
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")  # возвращает экземпляр с ответом
+
+
+def categories_by_slug(request, cat_slug):  # 3-я ф-я представления для вывода slug в категориях
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")  # возвращает slug и набор символов
+
+
+def archive(request, year):  # ф-я представления для archive
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")  # возвращает ответ ф-строку с годом(4 цифры)
