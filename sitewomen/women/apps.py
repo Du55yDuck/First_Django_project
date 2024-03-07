ff066b88cb5533e3aa02e56fd8041fc8f5029050
Denis Riabov <zwx7000@mail.ru>
2024-03-07 20:46:16 +0400

    Add admin panel. New classes, verbose_name. Register admin.

--- sitewomen/women/apps.py
+++ sitewomen/women/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class WomenConfig(AppConfig):
+    verbose_name = "Женщины мира"  # заголовок в админке
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'women'
