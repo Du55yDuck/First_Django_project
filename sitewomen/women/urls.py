ff100c1d65bc5eb6b31ebe9629c29a26202a3d00
Denis Riabov <zwx7000@mail.ru>
2024-03-21 23:54:33 +0400

    Add class WomenHome, WomenCategory(ListView).

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),  # маршрут для поля с собственным именем contact
     path('login/', views.login, name='login'),  # маршрут для поля с собственным именем login
     path('post/<slug:post_slug>/', views.show_post, name='post'),  # маршрут для шаблона и ф-ии post(орг-я ссылки)+slug
-    path('category/<slug:cat_slug>/', views.show_category, name='category'),  # маршрут с префиксом для category
+    path('category/<slug:cat_slug>/', views.WomenCategory.as_view(), name='category'),  # марш с префиксом для category
     path('tag/<slug:tag_slug>/', views.show_tag_postlist, name='tag'),  # маршрут tag/<slug..+ ф-я предст show_tag_pos..
 ]
