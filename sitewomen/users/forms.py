5537475b3a1fe261c1f3ca9faba7ce764d4495c1
Denis Riabov <zwx7000@mail.ru>
2024-04-08 23:48:35 +0400

    Add class RegisterUserForm, add register.html, add register_done. Add def register.

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -14,3 +14,30 @@ class LoginUserForm(AuthenticationForm):  # класс для авторизац
         # пользователя меняется, то get_user_model вернет указанную, вместо стандартной - good practice!)
         fields = ['username', 'password']  # поля для отображения
 
+
+class RegisterUserForm(forms.ModelForm):  # Класс форма для регистрации пользователя(связана с моделью)
+    username = forms.CharField(label="Логин")  # Атрибуты (поля для заполнения) - обязательное поле
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput())  # также обязательное поле
+    password2 = forms.CharField(label="Повтор пароль", widget=forms.PasswordInput())  # Поле для подтверждения пароля
+
+    class Meta:  # Вложенный класс (так как основной класс связан с моделью)
+        model = get_user_model()  # Возвращает текущую модель пользователя
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']  # поля отображаемые в форме
+        labels = {  # метки для полей
+            'email': 'E-mail',
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+        }
+
+        def clean_password2(self):  # Валидатор для проверки пароля на равенство
+            cd = self.cleaned_data  # словарь(очищенные данные)
+            if cd['password'] != cd['password2']:  # Проверка на равенство паролей
+                raise forms.ValidationError("Пароли не совпадают!")  # исключение, если пароли не совпадают
+            return cd['password']
+
+        def clean_email(self):  # Валидатор проверки на уникальность email-адреса
+            email = self.cleaned_data['email']  # данные с ключом по email
+            if get_user_model().objects.filter(email=email).exists():  # Из модели пользователя отобр польз-я по email,
+                # если такой найден, тогда отрабатывает генерация исключения
+                raise forms.ValidationError("Такой E-mail уже существует!")  # исключение, если проверка не прошла
+            return email
